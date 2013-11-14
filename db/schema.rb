e7e73abc171d0832fc603fca9bdf1dbcb66802d5
veleary <veleary@gmail.com>
2013-11-14 09:04:16 -0600

    include jqcloud assets

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131114143511) do
+ActiveRecord::Schema.define(version: 20131114142816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "apis", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "apps", force: true do |t|
     t.string   "name"
