605ebe978406272da65ba9a7e4ef24da1ba9ffe3
veleary <veleary@gmail.com>
2013-11-14 07:50:33 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WordCloud::Application.config.secret_key_base = '036afaabcfcddb5818469a86d2836d54150bab671f007da1371bd7377f6ff97192059693727a59997dae6239999e10bad74f6918063145d18fa06c59ed465f68'
